6c734570fad73b669ea0c70e87da0baf1f9f46d0
Leandro Bezerra <suporte@internetlojas.com>
2015-06-29 13:54:18 -0300

    novos arquivos de página para produtos atualizados

--- resources/views/produtos/pages/portfolio.blade.php
+++ resources/views/produtos/pages/portfolio.blade.php
@@ -2,10 +2,10 @@
 <div class="section">
     <!--============================== content =================================-->
     <div class="row">
-        <div class="col-md-3 col-lg-3">
+        <div class="col-md-2 col-lg-2">
             @include('layouts.includes.sidebar_left')
         </div>
-        <div class="col-md-9 col-lg-9">
+        <div class="col-md-10 col-lg-10">
             @include('layouts.contents.portfolio')  
         </div>
     </div>
